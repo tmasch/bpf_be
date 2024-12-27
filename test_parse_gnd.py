e4d4b097f4438d78d1ec216c22618cd5d8b454e4
bjgakress <bjgakress@gmail.com>
2024-12-27 15:08:54 +0100

    Create test_parse_gnd.py
    does not work!

---
+++ test_parse_gnd.py
@@ -0,0 +1,43 @@
+#pylint: disable=C0116
+"""
+Test of GND parsing
+"""
+from unittest import mock
+import pytest
+
+import test_get_external_data
+#import requests
+#import unittest
+#import json
+import pytest_asyncio
+#from beanie import Document, Link
+#from dotenv import load_dotenv
+#import logging
+
+
+import get_external_data
+import parse_gnd
+#import classes
+import db_actions
+from lxml import etree
+#import test_get_external_data
+
+#@mock.patch('get_external_data.get_web_data_as_json', side_effect=test_get_external_data.mock_get_web_data_as_json)
+@pytest.mark.asyncio
+#@classes.func_logger
+async def test_parse_gnd_name_preferred():
+    authority_id = "11860354X"
+    authority_url = r'https://services.dnb.de/sru/authorities?version=1.1&operation=searchRetrieve&query=NID%3D'\
+              + authority_id\
+                  + r'%20and%20BBG%3DTp*&recordSchema=MARC21-xml&maximumRecords=100'
+    result = []
+    content=await get_external_data.get_web_data(authority_url)
+    root = etree.XML(content)
+    records=root.find("records", namespaces=root.nsmap)
+#    print(records)
+#    print(records.tag)
+#    tag_id='400'
+#    subfield_code="a"
+    for record in records:
+        print("found record")
+    pass
