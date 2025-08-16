ce74a5e338e3de0d858128ec4a848dcbff84f104
bjgakress <bjgakress@gmail.com>
2025-08-16 17:45:31 +0200

    first new test

--- test_parse_gnd.py
+++ test_parse_gnd.py
@@ -34,12 +34,37 @@ async def create_test_record_rubens():
     r=await parse_gnd.get_records(gnd_id)
     return r[0]
 
+
+async def create_test_record_thomas_aquinas():
+    gnd_id="118622110"
+    r=await parse_gnd.get_records(gnd_id)
+    return r[0]
+
+
+
 #@mock.patch('get_external_data.get_web_data_as_json', side_effect=test_get_external_data.mock_get_web_data_as_json)
 @pytest.mark.asyncio
 #@classes.func_logger
-async def test_parse_gnd_name_preferred():
+async def test_parse_gnd_name_preferred_1():
+    """
+    Only subfield a filled
+    """
     await db_actions.initialise_beanie()
     record=await create_test_record_rubens()
     name_preferred=parse_gnd.gnd_record_get_name_preferred(record)
     assert name_preferred[0] == 'Rubens, Peter Paul'
     assert name_preferred[1] == ''
+
+
+@pytest.mark.asyncio
+#@classes.func_logger
+async def test_parse_gnd_name_preferred_2():
+    """
+    Only subfield a filled
+    """
+    await db_actions.initialise_beanie()
+    record=await create_test_record_thomas_aquinas()
+    name_preferred=parse_gnd.gnd_record_get_name_preferred(record)
+    assert name_preferred[0] == 'Thomas (von Aquin, Heiliger)'
+    assert name_preferred[1] == ''
+
